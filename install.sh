6d2463316a0a1bd7d2606266989217f40fd995bc
mjdubell <mjdubell@noreply.github.com>
2019-12-19 01:26:27 +0100

    updates

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 echo "[~] Installing bug bounty tools..."
-echo "======================================"
+echo "======================================\n"
 
-apt-get install git
+apt-get install git jq -y
 
 echo "[+] Installing golang..."
 wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz -O golang-13.5.tar.gz
 tar -C /usr/local -xzf golang-13.5.tar.gz
 rm -rf golang-13.5.tar.gz
 
+# SETUP GOPATH
+
 echo "[+] Adding /usr/local/go/bin to PATH..."
 echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
 echo "[+] Done, run source $HOME/.profile when install is done."
 
-# SETUP GOPATH
-
+echo "----------------------------"
 echo "[~] Installing go tools"
-echo "----------------------------
+echo "----------------------------"
 
 echo "[+] Installing gobuster..."
 go get github.com/OJ/gobuster
@@ -31,6 +32,9 @@ go get -u  go get -u github.com/tomnomnom/httprobe
 echo "[+] Installing meg..."
 go get -u github.com/tomnomnom/meg
 
+echo "[+] Installing unfurl..."
+go get -u github.com/tomnomnom/unfurl
+
 echo "[+] Installing aquatone..."
 AQUATONE="aquatone-1.7.0.zip"
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -O $AQUATONE
@@ -39,6 +43,17 @@ mv aquatone /usr/local/bin
 
 echo "[+] Installing gowitness..."
 go get -u github.com/sensepost/gowitness
+
+echo "----------------------------"
+echo "[~] Installing python tools"
+echo "----------------------------"
+
+echo "[+] Installing dnsgen..."
+pip install dnsgen
+
+echo "\n----------------------------"
+echo "[~] Installing misc tools"
+echo "------------------------------"
 
 echo "[+] Installing chromedriver to /opt/..."
 wget https://chromedriver.storage.googleapis.com/78.0.3904.105/chromedriver_linux64.zip -O chromedriver.zip
@@ -54,11 +69,3 @@ mv bin/massdns /usr/local/bin
 echo "[+] Installing SecLists to /opt/..."
 git clone https://github.com/danielmiessler/SecLists.git /opt/seclists
 
-echo "[+] Installing common2speak to /opt/wordlists/..."
-
-echo "----------------------------
-echo "[~] Installing python tools"
-echo "----------------------------
-
-echo "[+] Installing dnsgen..."
-pip install dnsgen
