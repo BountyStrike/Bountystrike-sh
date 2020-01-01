67abf685e3e98b059f6dd6864cc7cf93c09496cf
mjdubell <mjdubell@noreply.github.com>
2020-01-01 03:21:02 +0100

    u wot

--- bstrike.sh
+++ bstrike.sh
@@ -7,8 +7,8 @@
 
 VERSION="1.0"
 
-PROJECT=$1
-TARGET=$2
+PROJECT=""
+TARGET=""
 
 NOW=$(date +'%Y-%m-%d_%H-%M-%S')
 
@@ -17,13 +17,6 @@ GREEN="\033[1;32m"
 BLUE="\033[1;36m"
 YELLOW="\033[1;33m"
 RESET="\033[0m"
-
-if [ ! -d "$PROJECT" ]; then
-    mkdir $PROJECT
-    cd $PROJECT
-else
-    cd $PROJECT
-fi
 
 
 DOMAINS_FILE="domains-$NOW.txt"
@@ -60,9 +53,9 @@ certdata(){
 		crtsh=$(curl -s https://crt.sh/\?q\=%25$i%25.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a rawdata/$1-crtsh.txt )
 	done
 		#get a list of domains from certspotter
-		certspotter=$(curl -s https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep -w $1\$ | tee rawdata/$1-certspotter.txt)
+		certspotter=$(curl -s https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep -w $1\$ | tee $1-certspotter.txt)
 		#get a list of domains from digicert
-		digicert=$(curl -s https://ssltools.digicert.com/chainTester/webservice/ctsearch/search?keyword=$1 -o rawdata/$1-digicert.json) 
+		digicert=$(curl -s https://ssltools.digicert.com/chainTester/webservice/ctsearch/search?keyword=$1 -o $1-digicert.json) 
 		#echo "$crtsh"
 		#echo "$certspotter"
 		#echo "$digicert"
@@ -74,6 +67,7 @@ curl -s https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' 
 
 
 subdomainDiscovery() {
+    echo $TARGET
     runBanner "Subdomain Discovery with amass, subfinder and gobuster"
     # Passively find subdomains
     amass enum -passive -o $DOMAINS_FILE -log amass.log -d $TARGET &
@@ -122,7 +116,7 @@ contentDiscovery(){
     # maybe a second crawler would be good, to crawl the site now and specifically look for broken links to subdomain takeover services.
     # run against alive.txt to get all HTTP codes?
     runBanner "fff"
-    cat waybackurls.txt | fff -S -o htmlstorage
+    cat alive.txt | fff -S -o htmlstorage
 
     runBanner "GetJS"
     cat alive.txt | getJS -complete -output alive-js-files.txt
@@ -132,8 +126,6 @@ contentDiscovery(){
 
     ## meg
     # find a good wordlist to use for brutforcing with meg
-
-    ## Retire.js
 
     ## One liner to import whole list of subdomains into Burp suite for automated scanning!
     # cat <file-name> | parallel -j 200 curl -L -o /dev/null {} -x 127.0.0.1:8080 -k -s
@@ -165,36 +157,148 @@ vulnerabilityDiscovery(){
     runBanner "Subdomain takeover checks"
     subzy -targets $FINAL_DOMAINS | tee -a subtakeovers-$NOW.txt
 
-    # CRLF scanner
+    # CRLF scanner here
 
-    # open redirerct scanner 
+    # open redirerct scanner here
+
+    # github/gist search
+
+    # RetireJS here
 }
 
 
 # ------------------------------------------------------------------------- #
 
+help() {
 
-# Run pipeline
-# ==============================
-subdomainDiscovery
-contentDiscovery
-networkDiscovery
-vulnerabilityDiscovery
-visualDiscovery
+    echo "  ____   ____  _    _ _   _ _________     _______ _______ _____  _____ _  ________ ";
+    echo " |  _ \ / __ \| |  | | \ | |__   __\ \   / / ____|__   __|  __ \|_   _| |/ /  ____|";
+    echo " | |_) | |  | | |  | |  \| |  | |   \ \_/ / (___    | |  | |__) | | | | ' /| |__   ";
+    echo " |  _ <| |  | | |  | | . \` |  | |    \   / \___ \   | |  |  _  /  | | |  < |  __|  ";
+    echo " | |_) | |__| | |__| | |\  |  | |     | |  ____) |  | |  | | \ \ _| |_| . \| |____ ";
+    echo " |____/ \____/ \____/|_| \_|  |_|     |_| |_____/   |_|  |_|  \_\_____|_|\_\______|";
+    echo "                                                                                   ";
+    echo "                                 WHAT THE SHELL?                                   ";
+
+
+	echo """
+== Info
+ Bountystrike-sh is a simple bash pipeline script
+ containing a bunch tools piping data between each other.
+ No need for any fancy setup ^_^
+
+ Stiched together by @dubs3c.
+
+== Usage:
+	bstrike.sh <action> [project] [domain]
+	    bstrike.sh install
+	    bstrike.sh run google google.com
+	    bstrike.sh [assetdiscovery|ad] fra.se
+	    bstrike.sh [contentdiscovery|cd] fra.se
+	    bstrike.sh [networkdiscovery|nd] fra.se
+	    bstrike.sh [visualdiscovery|vd] fra.se
+	    bstrike.sh [vulndiscovery|vvd] fra.se
+	"""
+	exit 1
+}
+
+
+# main()
+# ======================
+if [[ $1 == "" ]] || [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+	help
+elif [[ $1 == "install" ]]; then
+
+    bash install.sh
+
+elif [[ $1 == "run" ]]; then
+
+	if [[ ! $2 == "" ]]; then
+        PROJECT=$2
+        if [ ! -d "$PROJECT" ]; then
+            mkdir $PROJECT
+            cd $PROJECT
+        else
+            cd $PROJECT
+        fi
+
+        if [[ $3 == "" ]]; then
+            echo "[-] Please specify a domain..."
+            help
+        fi
+
+        TARGET=$3
+
+        # Run pipeline
+        # ==============================
+        subdomainDiscovery
+        contentDiscovery
+        networkDiscovery
+        vulnerabilityDiscovery
+        visualDiscovery
+
+	else
+		help
+	fi
+elif [[ $1 == "assetdiscovery" ]] || [[ $1 == "ad" ]]; then
+        if [[ ! $2 == "" ]]; then
+            echo -e "${GREEN}[+] Running asset discovery on $2...${RESET}"
+            echo -e "${GREEN}==============================================================${RESET}"
+            TARGET=$2
+            subdomainDiscovery
+        else
+            help
+        fi
+elif [[ $1 == "contentdiscovery" ]] || [[ $1 == "cd" ]]; then
+        if [[ ! $2 == "" ]]; then
+            echo -e "${GREEN}[+] Running content discovery on $2...${RESET}"
+            echo -e "${GREEN}==============================================================${RESET}"
+            TARGET=$2
+            contentDiscovery
+        else
+            help
+        fi
+elif [[ $1 == "networkdiscovery" ]] || [[ $1 == "nd" ]]; then
+        if [[ ! $2 == "" ]]; then
+            echo -e "${GREEN}[+] Running network discovery on $2...${RESET}"
+            echo -e "${GREEN}==============================================================${RESET}"
+            TARGET=$2
+            networkDiscovery
+        else
+            help
+        fi
+elif [[ $1 == "vulndiscovery" ]] || [[ $1 == "vvd" ]]; then
+        if [[ ! $2 == "" ]]; then
+            echo -e "${GREEN}[+] Running vulnerability discovery on $2...${RESET}"
+            echo -e "${GREEN}==============================================================${RESET}"
+            TARGET=$2
+            vulnerabilityDiscovery
+        else
+            help
+        fi
+elif [[ $1 == "visualdiscovery" ]] || [[ $1 == "vd" ]]; then
+        if [[ ! $2 == "" ]]; then
+            echo -e "${GREEN}[+] Running visual discovery on $2...${RESET}"
+            echo -e "${GREEN}==============================================================${RESET}"
+            TARGET=$2
+            visualDiscovery
+        else
+            help
+        fi
+else
+    help
+fi
 
 echo -e "${GREEN}\n==== BountyStrike surface scan complete ====${RESET}"
 
 ## Stuff to fix
 # [x] create functions
 # [x] order functions by phase, i.e. asset discovery, content discovery, network discovery, vulnerability discovery
-# [] github, pastebin
+# [] github, gist
 # [] brute force URL paths
 # [] save header respones
 # [] install mullvad, use wireguard
-# [] use nahamsec rootdomains script?
 # [] database to store data
 # [] notification for new domains and diffs 
 # [] check for vulnerable javascript files
 # [] call api endpoint on found vuln --> notify via telegram
-
-
--- install.sh
+++ install.sh
@@ -309,6 +309,13 @@ else
     echo -e "${BOLD}${LIGHT_GREEN}[+] Installing api_wordlist to /opt/...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
 fi
 
+# TODO
+# https://github.com/fuzzdb-project/fuzzdb
+# https://github.com/berzerk0/Probable-Wordlists
+# https://github.com/s0md3v/Corsy
+# https://github.com/Bo0oM/fuzz.txt
+# Make sure to create amass configuration file
+
 if ! testcmd nmap; then
 
     echo -e "${BOLD}${LIGHT_GREEN}[+] Installing nmap...${NORMAL}"
